256651ec51a32426fdf19e2f8cc88bf522d007fa
admin <tananak.w@ku.th>
2019-01-01 06:02:44 +0700

    modified UI

---
+++ AccountSpring/src/main/java/AccountDao.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public interface AccountDao {
+    void insertAccount(Account account);
+    Account getOneAccount(int id);
+    List<Account> getAllAccounts();
+  //............. create also update and delete accounts
+    void updateAccount(int id, String name);
+    void deleteAccount(int id);
+}
